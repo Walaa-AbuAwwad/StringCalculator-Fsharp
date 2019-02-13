f652d367e9a54b7984915972ac6f869a78775f6d
Walaa AbuAwwad <walaa.abuawwad@gmail.com>
2019-02-14 01:45:04 +0200

    add zero,one or two num

---
+++ StringCalculator/Program.fs
@@ -0,0 +1,23 @@
+﻿ namespace StringCalculator
+
+
+ module calculator=
+
+  let getIntValue(number:string)=
+    if System.Text.RegularExpressions.Regex("^[0-9]+$").IsMatch number
+    then System.Int32.Parse(number)
+    else failwithf "Not A Number %s" number
+ 
+  let Add (numbers:string) =
+    match numbers.Length with 
+    | 0 -> 
+          0 
+    | _ ->
+          match numbers.Contains(",") with
+          |false ->
+             getIntValue(numbers) 
+          |true ->
+                let arrayOfNumbers= numbers.Split[|','|]
+                getIntValue(arrayOfNumbers.[0]) + getIntValue(arrayOfNumbers.[1])
+
+  
---
+++ StringCalculatorTest/Library1.fs
@@ -0,0 +1,25 @@
+﻿ module testModule
+ open StringCalculator
+ open NUnit.Framework
+
+
+ 
+ [<Test>]
+    let ``inValid Number``()=
+      Assert.DoesNotThrow(fun()-> (calculator.Add "77,7" |>ignore))
+      
+ [<Test>]
+    let ``when empty -> return 0``()=
+      let expected = calculator.Add ""
+      Assert.AreEqual(0,expected)
+
+ [<Test>]
+    let ``when one number -> return number``()=
+      let expected = calculator.Add "662"
+      Assert.AreEqual(662,expected)
+
+ [<Test>]
+    let ``Add two numbers splited by (,)``()=
+      let expected = calculator.Add "11,33"
+      Assert.AreEqual(44,expected)
+
